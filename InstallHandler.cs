bd583ad4f4062abd8e220e6e93dc76e7b87dbd04
ExaInsanity <nicolaiandorfer@outlook.com>
2021-07-21 14:30:41 +0200

    Create template installer

---
+++ InstallHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Xenon.InstallerLoader;
+
+namespace Template.Installer
+{
+	public class InstallHandler : IInstallHandler
+	{
+		public void Install(String version)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void PatchVersionMeta(String version) // mark as [Obsolete] if this is a server installer, keep the NIE. if this is a client, implement as usual.
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Installer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Xenon.InstallerLoader;
+using Xenon.InstallerLoader.Metadata;
+
+namespace Template.Installer
+{
+	public class Installer : IInstaller // the class must be public and cannot be abstract.
+	{
+		public String Id => throw new NotImplementedException();
+
+		public Xenon.TypeLib.Version Version => throw new NotImplementedException();
+
+		public InstallerEnvironment Environment => throw new NotImplementedException();
+
+		public void Disable()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Initialize()
+		{
+			throw new NotImplementedException();
+		}
+
+		public String[] ListTags()
+		{
+			throw new NotImplementedException();
+		}
+
+		public String[] ListVersions()
+		{
+			throw new NotImplementedException();
+		}
+
+		public String[] ListVersions(params String[] tags)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
