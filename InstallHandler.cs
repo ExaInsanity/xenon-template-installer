48bff5fd4d37d6358505deb94f5ad770b419c7e2
ExaInsanity <nicolaiandorfer@outlook.com>
2021-07-23 21:18:28 +0200

    update to Xenon.InstallerLoader 0.2.0

--- InstallHandler.cs
+++ InstallHandler.cs
@@ -6,12 +6,12 @@ namespace Template.Installer
 {
 	public class InstallHandler : IInstallHandler
 	{
-		public void Install(String version)
+		public void Install(String version, String installDirectory)
 		{
 			throw new NotImplementedException();
 		}
 
-		public void PatchVersionMeta(String version) // mark as [Obsolete] if this is a server installer, keep the NIE. if this is a client, implement as usual.
+		public void PatchVersionMeta(String version, String installDirectory) // mark as [Obsolete] if this is a server installer, keep the NIE. if this is a client, implement as usual.
 		{
 			throw new NotImplementedException();
 		}
